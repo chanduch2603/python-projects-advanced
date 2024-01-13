c630145f28ed187afc61153096588db579832eb3
chanduch2603 <155647568+chanduch2603@users.noreply.github.com>
2024-01-13 12:45:37 +0530

    Create runtime.py

---
+++ runtime.py
@@ -0,0 +1,19 @@
+from asyncio import run
+from time import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    Measure the excetution time of a subroutine
+    Args:
+        n (int): number of times to run the subroutine
+        max_delay (int): maximum amout of time to delay
+    Returns:
+        returns a float
+    """
+    if isinstance(n, int) and isinstance(max_delay, int):
+        c_time: float = time()
+        run(wait_n(n, max_delay))
+        return (time() - c_time) / n
+    raise TypeError('expected integer values to be passed!')
